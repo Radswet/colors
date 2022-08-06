591f74154f84a2942220daea1b0d5e04303cb07f
Radswet <fvergaralobos@gmail.com>
2022-08-05 23:26:00 -0400

    test ts

--- index.ts
+++ index.ts
@@ -1,1 +1,14 @@
-console.log("Hello World");
+import { Application, Router } from "https://deno.land/x/oak@v10.6.0/mod.ts";
+
+const router = new Router();
+const app = new Application();
+
+
+router.get("/", ({response}) => {
+    response.body = "Hello World";
+});
+
+app.use(router.routes());
+
+console.log("Server running on port 8000");
+await app.listen({ port: 8000 });
